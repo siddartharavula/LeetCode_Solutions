e7c84c3c14135f07ee7a7eeedd5eb7dbacb5dccf
Siddartha Ravula <siddartharavula2005@gmail.com>
2026-07-02 22:52:46 +0530

    Time: 55 ms (5.14%), Space: 22.6 MB (40.69%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans=[]
+        hm={}
+        for x in strs:
+            key="".join(sorted(x))
+            if key not in hm:
+                hm[key]=[x]
+            else:
+                hm[key].append(x)
+        for key,arr in hm.items():
+            ans.append(arr[:])
+        return ans
