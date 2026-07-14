c24741c19e8d90f97821cc751dea04e9b57c6c4c
Siddartha Ravula <siddartharavula2005@gmail.com>
2026-07-14 18:24:42 +0530

    Time: 271 ms (13.09%), Space: 40.3 MB (72.11%) - LeetHub

--- 3975-filter-occupied-intervals/3975-filter-occupied-intervals.py
+++ 3975-filter-occupied-intervals/3975-filter-occupied-intervals.py
@@ -17,23 +17,23 @@ class Solution:
         temp=[]
         for i in range(len(stack)):
             prev=stack[i]
-            if prev[1]<freeStart:
-                temp.append(prev)
-            elif prev[0]>=freeStart and prev[1]<=freeEnd:
-                continue
-            elif prev[0]>freeEnd:
-                temp.append(prev)
-            elif prev[0]<freeStart and prev[1]>freeEnd:
+            if prev[1]<freeStart: # before free time
+                temp.append(prev) 
+            elif prev[0]>=freeStart and prev[1]<=freeEnd: # in-between free time
+                continue  
+            elif prev[0]>freeEnd: # after free time
+                temp.append(prev) 
+            elif prev[0]<freeStart and prev[1]>freeEnd: # free time is in-between interval
                 start=prev[0]
                 end=freeStart-1
                 temp.append([start,end])
                 start=freeEnd+1
                 end=prev[1]
                 temp.append([start,end])
-            elif prev[0]<freeStart and prev[1]>=freeStart:
+            elif prev[0]<freeStart and prev[1]>=freeStart: # starts before but ends in-between free time
                 end=freeStart-1
                 temp.append([prev[0],end])
-            elif prev[0]>=freeStart and prev[1]>freeEnd:
+            elif prev[0]>=freeStart and prev[1]>freeEnd: # starts in-between buts end after free time
                 start=freeEnd+1
                 temp.append([start,prev[1]])
         return temp
