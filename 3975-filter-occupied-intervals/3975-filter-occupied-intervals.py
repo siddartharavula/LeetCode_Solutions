48f64ed0993819a086a1aa3ee72996dfa196df63
Siddartha Ravula <siddartharavula2005@gmail.com>
2026-07-14 18:20:43 +0530

    Time: 316 ms (5.07%), Space: 40.3 MB (72.11%) - LeetHub

---
+++ 3975-filter-occupied-intervals/3975-filter-occupied-intervals.py
@@ -0,0 +1,39 @@
+class Solution:
+    def filterOccupiedIntervals(self, occupiedIntervals: List[List[int]], freeStart: int, freeEnd: int) -> List[List[int]]:
+        n=len(occupiedIntervals)
+        occupiedIntervals.sort()
+        stack=[occupiedIntervals[0]]
+        for i in range(1,n):
+            prev=stack[-1]
+            cur=occupiedIntervals[i]
+            if prev[1]>=cur[0] or cur[0]-prev[1]==1:
+                stack.pop()
+                new_start=min(prev[0],cur[0])
+                new_end=max(prev[1],cur[1])
+                stack.append([new_start,new_end])
+            else:
+                stack.append(cur)
+        print(stack)
+        temp=[]
+        for i in range(len(stack)):
+            prev=stack[i]
+            if prev[1]<freeStart:
+                temp.append(prev)
+            elif prev[0]>=freeStart and prev[1]<=freeEnd:
+                continue
+            elif prev[0]>freeEnd:
+                temp.append(prev)
+            elif prev[0]<freeStart and prev[1]>freeEnd:
+                start=prev[0]
+                end=freeStart-1
+                temp.append([start,end])
+                start=freeEnd+1
+                end=prev[1]
+                temp.append([start,end])
+            elif prev[0]<freeStart and prev[1]>=freeStart:
+                end=freeStart-1
+                temp.append([prev[0],end])
+            elif prev[0]>=freeStart and prev[1]>freeEnd:
+                start=freeEnd+1
+                temp.append([start,prev[1]])
+        return temp
