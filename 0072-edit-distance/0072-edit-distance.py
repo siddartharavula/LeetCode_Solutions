1c2ab5faba820cb430071872bba16d72232a90c0
Siddartha Ravula <siddartharavula2005@gmail.com>
2026-07-23 00:12:13 +0530

    Time: 51 ms (47.1%), Space: 22.9 MB (35.29%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -2,6 +2,20 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         n=len(word1)
         m=len(word2)
+        dp=[[float(inf)]*(m+1) for _ in range(n+1)]
+        for i in range(m+1):
+            dp[0][i]=i
+        for i in range(n+1):
+            dp[i][0]=i
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    dp[i][j]=1+min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1])
+        return dp[n][m]
+        """
+        Resursion
         dp=[[float(inf)]*m for _ in range(n)]
         def find(i,j):
             if i<0:
@@ -15,4 +29,5 @@ class Solution:
                 return dp[i][j]
             dp[i][j]=min(1+find(i,j-1),1+find(i-1,j-1),1+find(i-1,j))
             return dp[i][j]
-        return find(n-1,m-1)        
+        return find(n-1,m-1)
+        """    
