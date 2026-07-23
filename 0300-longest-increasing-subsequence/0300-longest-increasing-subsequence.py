c54511ec9fe1b3b27d7f4145f3a815a6f8f300b1
Siddartha Ravula <siddartharavula2005@gmail.com>
2026-07-23 23:01:43 +0530

    Time: 4 ms (87.36%), Space: 19.5 MB (31.78%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n=len(nums)
+        import bisect # This module can be in sorted arrays
+        ans=[nums[0]]
+        for i in range(1,n):
+            if nums[i]>ans[-1]:
+                ans.append(nums[i])
+            else:
+                ind=bisect.bisect_left(ans,nums[i]) 
+                # bisect.bisect_left(array,value) -> returns the 1st index where we can put value in that array
+                ans[ind]=nums[i]
+        return len(ans)
+
+
+        """
+        Recursion
+        def find(i,prev):
+            if i<0:
+                return 0
+            not_take=find(i-1,prev)
+            take=0
+            if prev==n or nums[i]<nums[prev]:
+                take=1+find(i-1,i)
+            return max(take,not_take)
+        return find(n-1,n)
+        """
+        
