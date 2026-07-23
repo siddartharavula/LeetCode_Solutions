93fdb4de0e76377ec8dbe3f936e64a6acb551209
Siddartha Ravula <siddartharavula2005@gmail.com>
2026-07-23 19:10:49 +0530

    Time: 768 ms (22.33%), Space: 62.6 MB (21.17%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n=len(prices)
+        dp=[[[0]*3 for _ in range(2)]for _ in range(n+1)]
+        for ind in range(n-1,-1,-1):
+            for canBuy in range(2):
+                for flag in range(1,3):
+                    if canBuy==1:
+                        dp[ind][canBuy][flag]=max(-prices[ind]+dp[ind+1][0][flag],dp[ind+1][1][flag])
+                    else:
+                        dp[ind][canBuy][flag]=max(prices[ind]+dp[ind+1][1][flag-1],dp[ind+1][0][flag])
+        return dp[0][1][2]
+
+        """
+        Recursion
+        def find(ind,canBuy,flag):
+            if ind==n:
+                return 0
+            if flag==0:
+                return 0
+            if canBuy:
+                return max(-prices[ind]+find(ind+1,False,flag),find(ind+1,True,flag))
+            else:
+                return max(prices[ind]+find(ind+1,True,flag-1),find(ind+1,False,flag))
+        return find(0,True,2)
+        """
+        
