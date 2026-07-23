c1a647012c78e0418d7767c5fe7b012d5832b109
Siddartha Ravula <siddartharavula2005@gmail.com>
2026-07-23 19:29:43 +0530

    Time: 65 ms (40.47%), Space: 23.7 MB (39.46%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,7 +1,9 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n=len(prices)
-        k*=2
+        k*=2 # overall we have k*2 buy and sell things
+        # 1st base case, if ind==n then 0
+        # 2nd base case, if BuySell==k then 0
         dp=[[0]*(k+1) for _ in range(n+1)]
         for ind in range(n-1,-1,-1):
             for BuySell in range(k-1,-1,-1):
